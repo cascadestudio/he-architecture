3f120580e0d36bdf2f17449f86df651268436c26
Clément Lapasset <clement.lapasset@gmail.com>
2025-02-14 18:04:21 +0100

    restart project from scratch with ts

--- sanity/lib/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'next-sanity'
-
-export const client = createClient({
-  projectId: '5kwxuwwk',
-  dataset: 'production',
-  apiVersion: '2024-01-28',
-  useCdn: false,
-})
--- sanity/schemas/index.ts
+++
@@ -1,4 +0,0 @@
-// Add your schema types here
-import project from './project'
-
-export const schemaTypes = [project]
