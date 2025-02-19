3c095abc9316454a49f7f55fa536e9186d29f39b
Clément Lapasset <clement.lapasset@gmail.com>
2025-02-19 16:42:16 +0100

    strart home integration

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,28 @@
 import Link from "next/link";
-
+import Logo from "./logo/logo.svg";
+import Image from "next/image";
 export default function Header() {
   return (
-    <header className="py-6">
+    <header className="p-5">
       <nav>
-        <div className="flex justify-between items-center">
-          <Link href="/" className="text-2xl font-bold">
-            HE Architecture
+        <div className="">
+          <Link
+            href="/"
+            className="flex justify-between items-center lg:flex-col"
+          >
+            <Image
+              src={Logo}
+              alt="HE Architecture"
+              height={100}
+              width={100}
+              className="w-[80px] lg:w-[100px] h-auto lg:mb-11"
+            />
+            <div className="text-end lg:text-center">
+              <h1 className="text-2xl font-bold">HE Architecture</h1>
+              <p className="text-lg font-medium">Rénovation / Contemporain</p>
+            </div>
           </Link>
-          <div className="space-x-6">
+          <div className="space-x-6 hidden md:block">
             <Link href="/contact">Contact</Link>
           </div>
         </div>
